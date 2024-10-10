87c62709761c6e40ead0a0a7fdec330c718d7338
skindel <mjczepczynski@gmail.com>
2024-10-10 17:22:09 +0200

    Header anim done

--- new/js/head-anim.js
+++ new/js/head-anim.js
@@ -3,18 +3,32 @@ var prevScrollpos = window.scrollY;
 window.onscroll = function() {
 var currentScrollPos = window.scrollY;
 
-    var maxHeight = 15000; 
-    var minHeight = 13000; 
+    var maxCircumference = 15000; 
+    var minCircumference = 400;
+    var newCircumference;
+
+    var maxHeight = 200;
+    var minHeight = 150;
     var newHeight;
 
-    // Calculate new height based on scroll direction
+    var maxImgHeight = 100;
+    var minImgHeight = 50;
+    var newImgHeight;
+
+    // Calculate new Circumference based on scroll direction
     if (prevScrollpos > currentScrollPos) {
-        newHeight = maxHeight; // Scroll up
+        newCircumference = maxCircumference; // Scroll up
+        newHeight = maxHeight;
+        newImgHeight = maxImgHeight;
     } else {
-        newHeight = minHeight; // Scroll down
+        newCircumference = minCircumference; // Scroll down
+        newHeight = minHeight;
+        newImgHeight = minImgHeight;
     }
 
-    // Set the height CSS variable smoothly
-    document.documentElement.style.setProperty('--head-circumference', newHeight + 'px');
+    // Set the Circumference CSS variable smoothly
+    document.documentElement.style.setProperty('--head-circumference', newCircumference + 'px');
+    document.documentElement.style.setProperty('--head-height', newHeight + 'px');
+    document.documentElement.style.setProperty('--logo-height', newImgHeight + '%');
 prevScrollpos = currentScrollPos;
 }
